cc783c00cd3a3b8b50450abd38a6718089e2f19f
Patrick McGuire <insidenothing@gmail.com>
2021-03-31 13:16:38 -0400

    Create bloc.miner.sh

---
+++ crypto/mining/startup_scripts/bloc.miner.sh
@@ -0,0 +1,1 @@
+sudo xmrig --donate-level=1 -o blo.pool-pay.com:3393 -u abLoc6HBaYeHijGse3UDdVhCCCRi4Ct3aVrqeMhYWYUsG2qjfBCVN2jjAv17nNMEVEVWoHENMMacN29FpA1DdQVkHGC18ReiWfa.698 --rig-id=rpi2gb -p x@rpi2gb
