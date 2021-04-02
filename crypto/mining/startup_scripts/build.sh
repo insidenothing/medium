d9199701f8100b3bdb214859b60223c3ef2f0711
Patrick McGuire <insidenothing@gmail.com>
2021-04-01 20:22:29 -0400

    Update build.sh

--- crypto/mining/startup_scripts/build.sh
+++ crypto/mining/startup_scripts/build.sh
@@ -7,6 +7,6 @@ mkdir build
 cd build
 cmake ..
 make
-echo "./xmrig - donate-level=1 -o keva.bmorecoin.com:3333 -u VHDxcNZEzuwd1mx7LVivPXVdXFnk7oNnVQ - rig-id=t2micro -p x@t2micro" > start.sh
+echo "./xmrig --donate-level=1 -o keva.bmorecoin.com:3333 -u VHDxcNZEzuwd1mx7LVivPXVdXFnk7oNnVQ --rig-id=t2micro -p x@t2micro" > start.sh
 chmod +x start.sh
 ./start.sh
